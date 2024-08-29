26a8347cdbdb477d5638d4ac42bda84b4fed1256
andres-melgar <amelgar@pucp.edu.pe>
2024-08-29 07:00:54 -0500

    clase

--- 2024-2/cap02.b - punteros a punteros/cursos_de_alumnos/main.cpp
+++ 2024-2/cap02.b - punteros a punteros/cursos_de_alumnos/main.cpp
@@ -1,22 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
- */
-
-/* 
- * File:   main.cpp
- * Author: amelgar
- *
- * Created on 29 de agosto de 2024, 6:58
- */
-
-#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <assert.h>
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
     return 0;
