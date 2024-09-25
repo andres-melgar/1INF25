b0ade6428fe29920567a24f8cb4734e850df2054
andres-melgar <amelgar@pucp.edu.pe>
2024-09-25 08:51:01 -0500

    en clase

--- 2024-2/cap02.d - punteros_a_funciones/2024_1_Lab05/BibliotecaGenerica.cpp
+++ 2024-2/cap02.d - punteros_a_funciones/2024_1_Lab05/BibliotecaGenerica.cpp
@@ -11,11 +11,9 @@ void crealista(void *&pedidos, void *(*funcion_de_lectura)(ifstream &), const ch
 
     pedidos = nullptr;
     while (true) {
-        int dato;
-        archDatos >> dato;
+        void *ptrDato = funcion_de_lectura(archDatos);
         if (archDatos.eof())
             break;
-        void *ptrDato = retorna_referencia_a_entero(dato);
         insertalista(pedidos, ptrDato);
     }
     archDatos.close();
@@ -37,7 +35,7 @@ void insertalista(void *&pedidos, void *ptrDato) {
         //segundo caso, cuando la lista no está vacia
         void **lista = (void**) pedidos;
         void *fin_lista = lista[1];
-        void **ultimo_nodo = (void**)fin_lista;
+        void **ultimo_nodo = (void**) fin_lista;
         ultimo_nodo[1] = nuevo_nodo;
         lista[1] = nuevo_nodo;
     }
@@ -46,12 +44,55 @@ void insertalista(void *&pedidos, void *ptrDato) {
 void imprimelista(void *pedidos, void(*funcion_de_impresion)(ofstream &, void *), const char *nombre_de_archivo) {
     ofstream archReporte;
     AperturaDeUnArchivoDeTextosParaEscribir(archReporte, nombre_de_archivo);
-
-
-
+    if (pedidos != nullptr) {
+        void **lista = (void**) pedidos;
+        void *inicio_lista = lista[0];
+        void **nodo = (void**) inicio_lista;
+        //se hace el recorrido secuencial.
+        while (nodo != nullptr) {
+            void *ptrDato = nodo[0];
+            funcion_de_impresion(archReporte, ptrDato);
+            nodo = (void**) nodo[1];
+        }
+    }
     archReporte.close();
 }
 
 void combinalista(void *pedidos1, void *pedidos2, void *&pedidosfinal, bool(*funcion_de_comparacion)(void *, void*)) {
+    pedidosfinal = nullptr;
+    if (pedidos1 != nullptr and pedidos2 != nullptr) {
+        void **lista1 = (void**) pedidos1;
+        void **lista2 = (void**) pedidos2;
 
+        void *inicio_lista1 = lista1[0];
+        void *inicio_lista2 = lista2[0];
+
+        void **nodo1 = (void**) inicio_lista1;
+        void **nodo2 = (void**) inicio_lista2;
+
+        while (nodo1 != nullptr and nodo2 != nullptr) {
+            void *ptrDato1 = nodo1[0];
+            void *ptrDato2 = nodo2[0];
+            bool es_menor = funcion_de_comparacion(ptrDato1, ptrDato2);
+            if (es_menor) {
+                insertalista(pedidosfinal, ptrDato1);
+                nodo1 = (void**) nodo1[1];
+            } else{
+                insertalista(pedidosfinal, ptrDato2);
+                nodo2 = (void**) nodo2[1];
+            }
+        }
+        
+        while (nodo1 != nullptr){
+            void *ptrDato1 = nodo1[0];
+            insertalista(pedidosfinal, ptrDato1);
+            nodo1 = (void**) nodo1[1];
+        }
+        
+        while (nodo2 != nullptr){
+            void *ptrDato2 = nodo2[0];
+            insertalista(pedidosfinal, ptrDato2);
+            nodo2 = (void**) nodo2[1];
+        }
+    }
 }
--- 2024-2/cap02.d - punteros_a_funciones/2024_1_Lab05/FuncionesEnteras.cpp
+++ 2024-2/cap02.d - punteros_a_funciones/2024_1_Lab05/FuncionesEnteras.cpp
@@ -6,13 +6,20 @@
 using namespace std;
 
 void * leenum(ifstream &archivo_de_enteros) {
-
+    int dato;
+    archivo_de_enteros >> dato;
+    if (archivo_de_enteros.eof())
+        return nullptr;
+    return retorna_referencia_a_entero(dato);
 }
 
 void imprimenum(ofstream &archivo_de_enteros, void *ptrEntero) {
-
+    if (ptrEntero != nullptr)
+        archivo_de_enteros << *(int*) ptrEntero << endl;
 }
 
 bool cmpnum(void *ptrEnteroA, void *ptrEnteroB) {
-
+    int enteroA = *(int*)ptrEnteroA;
+    int enteroB = *(int*)ptrEnteroB;
+    return enteroA < enteroB;
 }
