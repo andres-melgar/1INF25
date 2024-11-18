2d7dfb948aa3c8043b13dada63e13772c93f1c0d
andres-melgar <amelgar@pucp.edu.pe>
2024-11-18 15:02:59 -0500

    clase

--- 2024-2/cap07.a - plantillas/PlantillaDeClases/Plantilla.hpp
+++ 2024-2/cap07.a - plantillas/PlantillaDeClases/Plantilla.hpp
@@ -5,6 +5,40 @@
 
 using namespace std;
 
+template <typename T>
+class Plantilla {
+private:
+    T dato;
+public:
+    void SetDato(T dato);
+
+    T GetDato();
+
+    void leer();
+
+    void imprimir();
+};
+
+template <typename T>
+void Plantilla<T>::SetDato(T dato){
+    this->dato = dato;
+}
+
+template <typename T>
+T Plantilla<T>::GetDato(){
+    return this->dato;
+}
+
+template <typename T>
+void Plantilla<T>::leer(){
+    cin >> this->dato;
+}
+
+template <typename T>
+void Plantilla<T>::imprimir(){
+    cout << this->dato;
+    cout << endl;
+}
 
 #endif /* PLANTILLA_HPP */
 
--- 2024-2/cap07.a - plantillas/PlantillaDeClases/main.cpp
+++ 2024-2/cap07.a - plantillas/PlantillaDeClases/main.cpp
@@ -17,15 +17,27 @@ int main(int argc, char** argv) {
 
 void test03(){
     cout << "Prueba: Plantilla<CPersona>" << endl;
+    class Plantilla<CPersona> objPersona;
+    CPersona persona("Melgar", "Sasieta", "Héctor Andrés");
+    objPersona.SetDato(persona);
+    objPersona.imprimir();
 }
 
 void test02() {
     cout << "Prueba: Plantilla<string>" << endl;
-
+    class Plantilla<string> objCadena;
+    objCadena.SetDato("Programación 2");
+    objCadena.imprimir();
+    //objCadena.leer();
+    //objCadena.imprimir();
 }
 
 void test01() {
     cout << "Prueba: Plantilla<int>" << endl;
-
+    class Plantilla<int> objEntero;
+    objEntero.SetDato(15);
+    objEntero.imprimir();
+    //objEntero.leer();
+    //objEntero.imprimir();
 }
 
