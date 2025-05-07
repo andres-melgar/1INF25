09d1cd876bc3e749848d3eab7ac64bfdf44232ff
andres-melgar <amelgar@pucp.edu.pe>
2025-05-07 10:02:53 -0500

    clase

--- sesión14 - punteros genéricos y punteros a funciones - lab05 - 2024-2/2024_1_Lab05_Dev/BibliotecaGenerica.cpp
+++ sesión14 - punteros genéricos y punteros a funciones - lab05 - 2024-2/2024_1_Lab05_Dev/BibliotecaGenerica.cpp
@@ -4,33 +4,94 @@
 
 using namespace std;
 
-void crealista(void *&pedidos, void *(*funcion_de_lectura)(ifstream &), const char *nombre_de_archivo) {
+void crealista(void *&pedidos, void *(*funcion_de_lectura)(ifstream &),
+        const char *nombre_de_archivo) {
     ifstream archDatos;
     AperturaDeUnArchivoDeTextosParaLeer(archDatos, nombre_de_archivo);
 
-    
+    pedidos = nullptr;
     while (true) {
-        
+        void *ptrDato = funcion_de_lectura(archDatos);
         if (archDatos.eof())
             break;
-        
+        insertalista(pedidos, ptrDato);
     }
     archDatos.close();
 }
 
 void insertalista(void *&pedidos, void *ptrDato) {
-    
+    void **nuevo_nodo = new void*[2];
+    nuevo_nodo[0] = ptrDato;
+    nuevo_nodo[1] = nullptr;
+
+    //caso particular: la lista está vacio
+    if (pedidos == nullptr) {
+        void **lista = new void*[2];
+        lista[0] = nuevo_nodo;
+        lista[1] = nuevo_nodo;
+        pedidos = lista;
+    } else {
+        void **lista = (void**) pedidos;
+        void *fin_lista = lista[1]; //sacando nodo color marrón en la gráfica
+        void **ultimo_nodo = (void**) fin_lista; //sacando nodo color marrón en la gráfica
+        ultimo_nodo[1] = nuevo_nodo;
+        lista[1] = nuevo_nodo;
+    }
 }
 
-void imprimelista(void *pedidos, void(*funcion_de_impresion)(ofstream &, void *), const char *nombre_de_archivo) {
+void imprimelista(void *pedidos, void(*funcion_de_impresion)(ofstream &, void *),
+        const char *nombre_de_archivo) {
     ofstream archReporte;
     AperturaDeUnArchivoDeTextosParaEscribir(archReporte, nombre_de_archivo);
-   
-    
-    
+    //recorrido secuencial
+    if (pedidos != nullptr) {
+        void **lista = (void**) pedidos;
+        void *inicio_lista = lista[0]; //sacando nodo color verde en la gráfica
+        void **nodo = (void**) inicio_lista; //sacando nodo color verde en la gráfica
+        while (nodo != nullptr) {
+            void *ptrDato = nodo[0];
+            funcion_de_impresion(archReporte, ptrDato);
+            nodo = (void**) nodo[1];
+        }
+    }
     archReporte.close();
 }
 
 void combinalista(void *pedidos1, void *pedidos2, void *&pedidosfinal, bool(*funcion_de_comparacion)(void *, void*)) {
-    
+    pedidosfinal = nullptr;
+    if (pedidos1 != nullptr and pedidos2 != nullptr) {
+        void **lista1 = (void**) pedidos1;
+        void **lista2 = (void**) pedidos2;
+
+        void *inicio_lista1 = lista1[0];
+        void *inicio_lista2 = lista2[0];
+
+        void **nodo1 = (void**) inicio_lista1;
+        void **nodo2 = (void**) inicio_lista2;
+
+        while (nodo1 != nullptr and nodo2 != nullptr) {
+            void *ptrDato1 = nodo1[0];
+            void *ptrDato2 = nodo2[0];
+            bool es_menor = funcion_de_comparacion(ptrDato1, ptrDato2);
+            if (es_menor) {
+                insertalista(pedidosfinal, ptrDato1);
+                nodo1 = (void**) nodo1[1];
+            } else {
+                insertalista(pedidosfinal, ptrDato2);
+                nodo2 = (void**) nodo2[1];
+            }
+        }
+
+        while (nodo1 != nullptr) {
+            void *ptrDato1 = nodo1[0];
+            insertalista(pedidosfinal, ptrDato1);
+            nodo1 = (void**) nodo1[1];
+        }
+        
+        while (nodo2 != nullptr) {
+            void *ptrDato2 = nodo2[0];
+            insertalista(pedidosfinal, ptrDato2);
+            nodo2 = (void**) nodo2[1];
+        }
+    }
 }
--- sesión14 - punteros genéricos y punteros a funciones - lab05 - 2024-2/2024_1_Lab05_Dev/FuncionesRegistros.cpp
+++ sesión14 - punteros genéricos y punteros a funciones - lab05 - 2024-2/2024_1_Lab05_Dev/FuncionesRegistros.cpp
@@ -6,13 +6,40 @@
 using namespace std;
 
 void *leeregistro(ifstream &archivo_de_registro) {
+    int pedido_buffer, dni_buffer;
+    char codigo_buffer[8];
 
+    archivo_de_registro>>pedido_buffer;
+    if (archivo_de_registro.eof())
+        return nullptr;
+    archivo_de_registro.get();
+    archivo_de_registro>>dni_buffer;
+    archivo_de_registro.get();
+    archivo_de_registro.getline(codigo_buffer, 8);
+
+    void **bloque_registro = new void*[3];
+    bloque_registro[0] = retorna_referencia_a_entero(pedido_buffer);
+    bloque_registro[1] = retorna_referencia_a_entero(dni_buffer);
+    bloque_registro[2] = mi_strdup(codigo_buffer);
+    return bloque_registro;
 }
 
 void imprimeregistro(ofstream &archivo_de_registro, void *ptrRegistro) {
-
+    if (ptrRegistro != nullptr) {
+        void **bloque_registro = (void**) ptrRegistro;
+        if (bloque_registro != nullptr) {
+            int *pedido = (int*) bloque_registro[0];
+            int *dni = (int*) bloque_registro[1];
+            char *codigo = (char*) bloque_registro[2];
+            archivo_de_registro << *pedido << " - " << *dni << " - " << codigo << endl;
+        }
+    }
 }
 
 bool cmpregistro(void *ptrRegistroA, void *ptrRegistroB) {
-
+    void **bloque_registroA = (void**) ptrRegistroA;
+    void **bloque_registroB = (void**) ptrRegistroB;
+    int pedidoA = *(int*) bloque_registroA[0];
+    int pedidoB = *(int*) bloque_registroB[0];
+    return pedidoA<pedidoB;
 }
