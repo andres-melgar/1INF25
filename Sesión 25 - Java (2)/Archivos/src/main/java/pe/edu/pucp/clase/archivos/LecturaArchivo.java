cf324bf3d13d2c7e11545897c4a5f511d7aee7fd
Andrés Melgar <amelgar@pucp.edu.pe>
2025-11-17 11:00:35 -0500

    clase

---
+++ Sesión 25 - Java (2)/Archivos/src/main/java/pe/edu/pucp/clase/archivos/LecturaArchivo.java
@@ -0,0 +1,9 @@
+package pe.edu.pucp.clase.archivos;
+
+class LecturaArchivo {
+
+    static void leer_archivo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
